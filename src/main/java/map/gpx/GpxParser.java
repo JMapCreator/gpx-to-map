9e861c192533321a510f7cdb4829b2e0fa77c4a1
Ivan <ivan.bethus@gmail.com>
2024-10-05 16:58:32 +0200

    fix: fix map concat ordrer

--- src/main/java/map/gpx/GpxParser.java
+++ src/main/java/map/gpx/GpxParser.java
@@ -7,9 +7,11 @@ import io.jenetics.jpx.WayPoint;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Instant;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class GpxParser {
@@ -31,12 +33,31 @@ public class GpxParser {
                 .map(File::toPath)
                 .map(GpxParser::readGpx)
                 .flatMap(GPX::tracks)
+                .sorted(getTrackComparator())
                 .forEach(gpxBuilder::addTrack);
         GPX gpx = gpxBuilder.build();
         final var indent = new GPX.Writer.Indent("    ");
         Path concatenatedFilePath = outputFolder.resolve("concatenated.gpx");
         GPX.Writer.of(indent).write(gpx, concatenatedFilePath);
         return concatenatedFilePath.toFile();
+    }
+
+    private static Comparator<Track> getTrackComparator() {
+        return (t1, t2) -> {
+            Optional<WayPoint> firstWp1 = t1.segments().flatMap(TrackSegment::points).min(getWayPointComparator());
+            Optional<WayPoint> firstWp2 = t2.segments().flatMap(TrackSegment::points).min(getWayPointComparator());
+            Instant wpT1 = firstWp1.map(wp -> wp.getTime().orElse(Instant.EPOCH)).orElse(Instant.EPOCH);
+            Instant wpT2 = firstWp2.map(wp -> wp.getTime().orElse(Instant.EPOCH)).orElse(Instant.EPOCH);
+            return wpT1.compareTo(wpT2);
+        };
+    }
+
+    private static Comparator<WayPoint> getWayPointComparator() {
+        return (p1, p2) -> {
+            Instant t1 = p1.getTime().orElse(Instant.EPOCH);
+            Instant t2 = p2.getTime().orElse(Instant.EPOCH);
+            return t1.compareTo(t2);
+        };
     }
 
     private static GPX readGpx(Path path) {
